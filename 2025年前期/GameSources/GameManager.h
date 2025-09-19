acc84b808754066e9889d08c430d67d8b50ebe1a
Munakata Haruto <zeal23420039@fsgcl.jp>
2025-09-19 10:17:26 +0900

    GameManagerに手札、マップの情報を追加

--- 2025年前期/GameSources/GameManager.h
+++ 2025年前期/GameSources/GameManager.h
@@ -7,13 +7,17 @@
 #include "stdafx.h"
 
 namespace basecross{
-
+	class GimmickHand;
 
 	class GameManager : public SingletonBase<GameManager> {
 		friend class SingletonBase<GameManager>;
 		GameManager() = default;
 
 		vector<shared_ptr<MoveSphere>> m_Spheres;
+
+		shared_ptr<GimmickHand> m_Hand;
+
+		shared_ptr<Map> m_Map;
 	public:
 		void AddSphere(const shared_ptr<MoveSphere> sphere) {
 			m_Spheres.push_back(sphere);
@@ -21,6 +25,22 @@ namespace basecross{
 		vector<shared_ptr<MoveSphere>> GetSpheres()const {
 			return m_Spheres;
 		}
+		void SetHand(shared_ptr<GimmickHand> hand) {
+			m_Hand = hand;
+		}
+
+		shared_ptr<GimmickHand> GetHand()const {
+			return m_Hand;
+		}
+
+		void SetMap(shared_ptr<Map> map) {
+			m_Map = map;
+		}
+
+		shared_ptr<Map> GetMap() const{
+			return m_Map;
+		}
+		void Update();
 	};
 }
 //end basecross
