2e7db2d41fa7620c8c01c24109a044cbc8134b42
Yushin Okawara <zeal23420135@fsgcl.jp>
2025-10-22 14:20:06 +0900

    リザルト処理追加

--- 2025年前期/GameSources/GameManager.cpp
+++ 2025年前期/GameSources/GameManager.cpp
@@ -41,6 +41,19 @@ namespace basecross{
 		}
 	}
 
+	void GameManager::ResultUpdate()
+	{
+		if (!CompareState(GameState::Clear)) return;
+
+		m_ResultTime += App::GetApp()->GetElapsedTime();
+
+		if (m_ResultTime >= 0.05f)
+		{
+			ResultCreate();
+			m_ResultTime = 0.0f;
+		}
+	}
+
 	void GameManager::DrawTempGimmicks() {
 		
 	}
@@ -62,12 +75,13 @@ namespace basecross{
 	void GameManager::Update() {
 		m_KeyConfigFile.Load(L"Json/keyconfig.json");
 		InputUpdate();
-		
+		ResultUpdate();
 		//ここから下はゲーム進行中の処理
 		if (!CompareState(GameState::Game)) return;
 
 		m_Tick += App::GetApp()->GetElapsedTime();
 		if (m_Tick <= m_UpdateTicks) return;
+
 
 		if (!IsUpdate()) return;
 		m_Tick = 0;
@@ -77,6 +91,7 @@ namespace basecross{
 	}
 
 	void GameManager::InputUpdate() {
+
 
 		auto& input = InputManager::GetInputManager();
 		if (input->GetDownButton(GetKeyConfig(L"toolBack"))) {
@@ -104,10 +119,17 @@ namespace basecross{
 
 	void GameManager::DrawGoalEffect() {
 		if (!CompareState(GameState::Game)) return;
-		DeleteEffectSprite();
-		auto sprite = m_Stage->AddGameObject<Sprite>(L"TEMP_GOAL_SPRITE",Vec3(), Vec2(500, 250), Anchor::Center);
-		m_EffectSprite.push_back(sprite);
+
 		m_GameState = GameState::Clear;
+
+		auto menu = m_Stage->GetChileStageVec()[0];
+
+		auto& app = App::GetApp();
+
+		auto backBoardUI = menu->AddGameObject<Sprite>(L"ResultBackBoardUI", Vec3(0.0f, 0.0f, 0.0f), Vec2(600, 600), Anchor::Center);
+		auto starCoverUI = menu->AddGameObject<Sprite>(L"StarCoverUI", Vec3(0.0f, 0.0f, 0.0f), Vec2(600, 180), Anchor::Center);
+		m_EffectSprite.push_back(backBoardUI);
+		m_EffectSprite.push_back(starCoverUI);
 
 		StopCube();
 	}
@@ -120,5 +142,84 @@ namespace basecross{
 
 		StopCube();
 	}
+
+
+	void GameManager::ResultCreate()
+	{
+		if (m_StarSp.size() == 0)
+		{
+			auto menu = m_Stage->GetChileStageVec()[0];
+			for (int i = 0; i < 3; i++)
+			{
+				auto starUI = menu->AddGameObject<Sprite>(L"StarUI", Vec3(-200, 0.0f, 0.0f), Vec2(200, 180), Anchor::Center);
+				starUI->SetDiffuse(Col4(1.0f, 1.0f, 1.0f, 0.0f));
+				starUI->CreateAnimationUV(Vec2(3, 1));
+
+				auto evaluationUI = menu->AddGameObject<Sprite>(L"EvaluationUI", Vec3(0.0f, 0.0f, 0.0f), Vec2(500.0f, 78.0f), Anchor::Top);
+				evaluationUI->SetDiffuse(Col4(1.0f, 1.0f, 1.0f, 1.0f));
+				evaluationUI->CreateAnimationUV(Vec2(1, 3));
+
+				if (i == 1)
+				{
+					starUI->SetPosition(Vec3(200, 0.0f, 0.0f));
+					auto uv = starUI->GetUv(2);
+					starUI->UpdateUV(uv);
+					m_StarSp.push_back(starUI);
+					m_EffectSprite.push_back(starUI);
+				}
+
+				else if (i == 2)
+				{
+					starUI->SetPosition(Vec3(0.0f, 0.0f, 0.0f));
+					auto uv = starUI->GetUv(1);
+					starUI->UpdateUV(uv);
+					m_StarSp.push_back(starUI);
+					m_EffectSprite.push_back(starUI);
+				}
+				else
+				{
+					auto uv = starUI->GetUv(i);
+					starUI->UpdateUV(uv);
+					m_StarSp.push_back(starUI);
+					m_EffectSprite.push_back(starUI);
+				}
+
+				evaluationUI->SetPosition(Vec3(0.0f, 80.0f - 60.0f * i, 0.0f));
+				auto uv = evaluationUI->GetUv(i);
+				evaluationUI->UpdateUV(uv);
+				m_EvaluationSp.push_back(evaluationUI);
+				m_EffectSprite.push_back(evaluationUI);
+
+			}
+
+
+			m_CurrentStarIndex = 0;
+
+		}
+		if (m_CurrentStarIndex >= m_StarSp.size()) return;
+
+		if (m_GameEvaluation <= 0) return;
+
+		auto star = m_StarSp[m_CurrentStarIndex];
+		Col4 current = star->GetDiffuse();
+		float alpha = current.getW();
+		Vec2 currentSize = star->GetSize();
+
+		// フェードイン
+		if (alpha < 1.0f)
+		{
+			alpha += 0.15f;
+			if (alpha > 1.0f) alpha = 1.0f;
+			star->SetDiffuse(Col4(1.0f, 1.0f, 1.0f, alpha));
+
+			m_EvaluationSp[m_CurrentStarIndex]->SetDiffuse(Col4(1.0f, 0.0f, 0.0f, 1.0f));
+		}
+		else
+		{
+			m_CurrentStarIndex++;
+			m_GameEvaluation--;
+		}
+	}
+
 }
 //end basecross
--- 2025年前期/GameSources/GameManager.h
+++ 2025年前期/GameSources/GameManager.h
@@ -43,6 +43,17 @@ namespace basecross{
 		void GimmickUpdate();
 		void CubeUpdate();
 		void StopCube();
+
+		//ƒŠƒUƒ‹ƒg
+		void ResultCreate();
+		void ResultUpdate();
+		float m_ResultTime;
+		int m_CurrentStarIndex = 0;
+		int m_GameEvaluation = 2;
+		vector<shared_ptr<Sprite>> m_StarSp;
+		vector<shared_ptr<Sprite>> m_EvaluationSp;
+
+
 	public:
 		/// <summary>
 		/// “o˜^‚³‚ê‚Ä‚¢‚éî•ñ‚ğ‰Šú‰»‚·‚é
@@ -154,6 +165,13 @@ namespace basecross{
 			}
 			m_EffectSprite.clear();
 		}
+
+
+		void SetGameEvaluation(int eval)
+		{
+			m_GameEvaluation = eval;
+		}
+
 	};
 }
 //end basecross
