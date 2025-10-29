5f185c73dba1309945b75d086b53e50d827a7920
Munakata Haruto <zeal23420039@fsgcl.jp>
2025-10-29 14:09:49 +0900

    テクスチャミスを修正

--- 2025年前期/GameSources/GameManager.cpp
+++ 2025年前期/GameSources/GameManager.cpp
@@ -78,7 +78,7 @@ namespace basecross{
 
 	void GameManager::StartFade() {
 		if (!m_SpriteFade) {
-			auto sprite = m_MenuStage->AddGameObject<Sprite>(L"EXPAIN_PL",Vec3(),Vec2(),Anchor::Center);
+			auto sprite = m_MenuStage->AddGameObject<Sprite>(L"ICON_KILL",Vec3(),Vec2(),Anchor::Center);
 			sprite->MatchToScreenSize();
 			sprite->SetDiffuse(Col4(0, 0, 0, 1));
 			sprite->SetLayer(10);
