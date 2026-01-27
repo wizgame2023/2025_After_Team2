90b8844da51d8e810c51e7614f7a2a064f991472
Munakata Haruto <zeal23420039@fsgcl.jp>
2026-01-27 14:08:36 +0900

    丸め忘れを解消

--- 2025年前期/GameSources/Player.cpp
+++ 2025年前期/GameSources/Player.cpp
@@ -150,7 +150,7 @@ namespace basecross{
 		}
 		Vec3 currentPosition = GetPosition();
 		auto& level = GameManager::GetInstance().GetLevelManager();
-		auto mapData = level->GetMap()->GetMapData(Vec2(currentPosition.x, -currentPosition.z));
+		auto mapData = level->GetMap()->GetMapData(Vec2(round(currentPosition.x), round(-currentPosition.z)));
 
 		if (mapData.m_ColorStr == L"black") {
 			return false;
