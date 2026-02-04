b8f5c9a64caa3da1ccfd4821913bffd88e171e16
Hiroto Nakanishi <zeal23420130@fsgcl.jp>
2026-02-04 11:24:46 +0900

    ヘルプの切り替え

--- 2025年前期/GameSources/Menu.cpp
+++ 2025年前期/GameSources/Menu.cpp
@@ -123,6 +123,7 @@ namespace basecross{
 		m_PoseMenu->Close();
 	}
 	void GameMenu::OnUpdate() {
+		m_OperateInfo->SetDrawActive(false);
 		auto& input = InputManager::GetInputManager();
 		auto& gameManager = GameManager::GetInstance();
 
